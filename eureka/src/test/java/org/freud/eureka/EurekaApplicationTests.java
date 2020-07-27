323d40ed3eae1716584f15bc49624c24cbf1bd87
FreudFan <32455840+FreudFan@users.noreply.github.com>
2020-07-27 18:31:24 +0800

    搭建eureka注册中心

---
+++ eureka/src/test/java/org/freud/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.freud.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
