d8c071ca7db8a53d1fd1a26b605bc8ddbb0f2326
FreudFan <32455840+FreudFan@users.noreply.github.com>
2020-07-28 21:33:50 +0800

    初始化zuul网关服务

---
+++ api-gateway/src/main/java/org/freud/apigateway/filter/AuthFilter.java
@@ -0,0 +1,41 @@
+package org.freud.apigateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_DECORATION_FILTER_ORDER;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+@Component
+public class AuthFilter extends ZuulFilter {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    @Autowired
+    private HttpServletRequest request;
+
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return PRE_DECORATION_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+
+        return null;
+    }
+}
