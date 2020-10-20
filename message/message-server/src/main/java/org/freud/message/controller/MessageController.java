19f26b0f8cb8ef19ee10c61f0bd0b2e73bfba2e8
FreudFan <32455840+FreudFan@users.noreply.github.com>
2020-10-20 22:31:14 +0800

    实现message模块查询消息记录

---
+++ message/message-server/src/main/java/org/freud/message/controller/MessageController.java
@@ -0,0 +1,65 @@
+package org.freud.message.controller;
+
+import com.github.pagehelper.PageInfo;
+import org.freud.message.common.ChatMsg;
+import org.freud.message.entity.ChatMessage;
+import org.freud.message.entity.GroupMessage;
+import org.freud.message.interceptor.RequestContent;
+import org.freud.message.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/message")
+public class MessageController {
+    @Autowired
+    private MessageService chatMessageService;
+    @Autowired
+    private MessageService groupMessageService;
+
+    @GetMapping("/unread")
+    public List<ChatMsg> getUnReadMsgList() {
+        int currentId = RequestContent.getCurrentUser().getId();
+        return chatMessageService.getUnReadMsgList(currentId);
+    }
+
+    /***
+     * 分页查询用户聊天记录
+     * @param userId 目标用户id
+     * @param pageNum 页数
+     * @param size 单页个数 默认20
+     * @param startDate 开始查询时间 如果有的话是正序查询
+     * @return
+     */
+    @GetMapping(value = {
+            "/chat/{userId}/{pageNum}",
+            "/chat/{userId}/{pageNum}/{size}"})
+    public PageInfo<ChatMessage> getChatMessageList(@PathVariable("userId") Integer userId,
+                                                    @PathVariable("pageNum") Integer pageNum,
+                                                    @PathVariable(name = "size", required = false) Integer size,
+                                                    @RequestParam(name = "startDate", required = false) LocalDate startDate) {
+        return chatMessageService.getHistoryChatMessage(userId, pageNum, size, startDate);
+    }
+
+    /***
+     * 分页查询群聊天记录
+     * @param groupId 目标群id
+     * @param pageNum 页数
+     * @param size 单页个数 默认20
+     * @param startDate 开始查询时间 如果有的话是正序查询
+     * @return
+     */
+    @GetMapping(value = {
+            "/group/{groupId}/{pageNum}",
+            "/group/{groupId}/{pageNum}/{size}"})
+    public PageInfo<GroupMessage> getGroupMessageList(@PathVariable("groupId") Integer groupId,
+                                                      @PathVariable("pageNum") Integer pageNum,
+                                                      @PathVariable(name = "size", required = false) Integer size,
+                                                      @RequestParam(name = "startDate", required = false) LocalDate startDate) {
+        return groupMessageService.getHistoryGroupMessage(groupId, pageNum, size, startDate);
+    }
+
+}
