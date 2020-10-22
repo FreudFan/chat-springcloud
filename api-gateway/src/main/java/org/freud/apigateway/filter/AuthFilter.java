8ac12b90cd333a9a138a3b2a046e5aefca007239
FreudFan <32455840+FreudFan@users.noreply.github.com>
2020-10-22 23:19:11 +0800

    Update AuthFilter.java

--- api-gateway/src/main/java/org/freud/apigateway/filter/AuthFilter.java
+++ api-gateway/src/main/java/org/freud/apigateway/filter/AuthFilter.java
@@ -1,16 +1,10 @@
 package org.freud.apigateway.filter;
 
 import com.netflix.zuul.ZuulFilter;
-import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 
 import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_DECORATION_FILTER_ORDER;
 import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
