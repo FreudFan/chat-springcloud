d336752a641201a75f81d172bfcfdea1edd26013
FreudFan <32455840+FreudFan@users.noreply.github.com>
2020-11-21 16:37:38 +0800

    api-gateway应用加入apollo配置中心

--- api-gateway/src/main/java/org/freud/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/org/freud/apigateway/ApiGatewayApplication.java
@@ -1,10 +1,12 @@
 package org.freud.apigateway;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableApolloConfig
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
